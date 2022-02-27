1c9f1064279a417010bf8b816efd4549afac9450
Lewis McNeill <6698050+lewis785@users.noreply.github.com>
2022-02-27 17:18:53 +0000

    ADD json example to example-file command

--- src/cli/commands/exampleFile.ts
+++ src/cli/commands/exampleFile.ts
@@ -1,27 +1,54 @@
 import { Options } from 'yargs'
 import { writeFileSync } from 'fs'
+import { Input } from '../../maldi/types'
 
 interface Arguments {
   format: string
 }
 
-export const command = 'example-file'
+export const command = 'example-file [format]'
 export const describe = 'Generate example import file'
 
 export const builder: { [key: string]: Options } = {
   format: {
     nargs: 1,
+    demandOption: true,
     describe: 'Format of example import file',
-    choices: ['csv'],
-    default: 'csv',
+    choices: ['csv', 'json'],
   },
 }
 
 export const handler = (argv: Arguments) => {
-  writeFileSync('example_import.csv', exampleCsv)
+  switch (argv.format) {
+    case 'csv':
+      writeFileSync('example_import.csv', exampleCsv)
+      break
+    case 'json':
+      writeFileSync('example_import.json', JSON.stringify(exampleJson, null, 2))
+      break
+    default:
+      throw Error(`Unsupported file type: ${argv.format}`)
+  }
 }
 
 const exampleCsv = `Peak Name,Peak Mass,Monomer Name,Monomer Mass,Cation Name,Cation Mass,Endgroup Name,Endgroup Mass
 Peak A,123,Monomer A,111,Cation A,25,Endgroup A,1
 Peak B,234,,,Cation B,50,Endgroup B,2
 ,,,,,,Endgroup C,3`
+
+const exampleJson: Input = {
+  peaks: [
+    { name: 'Peak A', mass: 123 },
+    { name: 'Peak B', mass: 321 },
+  ],
+  monomers: [{ name: 'Monomer A', mass: 111 }],
+  cations: [
+    { name: 'Cation A', mass: 25 },
+    { name: 'Cation B', mass: 50 },
+  ],
+  endGroups: [
+    { name: 'Endgroup A', mass: 1 },
+    { name: 'Endgroup B', mass: 2 },
+    { name: 'Endgroup B', mass: 3 },
+  ],
+}
--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,15 +1,12 @@
 #!/usr/bin/env node
 
-import { importer } from '../csv/importer'
-import { generateMaldiResults } from '../maldi'
 import yargs from 'yargs'
-import { exporter } from '../csv/exporter'
 
-const argv = yargs
+yargs
   .usage('Usage: $0 <command> [options]')
   .commandDir('commands')
   .demandCommand()
-  .help('h')
-  .example('$0 example-file', 'Generate an example csv file')
+  .help()
+  .example('$0 example-file csv', 'Generate an example csv file')
   .example('$0 analyse input.csv -t 10 -o test-result', 'Run analysis on file')
   .alias('help', 'help').argv
