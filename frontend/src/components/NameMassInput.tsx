058567b70ec8c02846e474ba72412dc390b4aa74
Lewis McNeill <6698050+lewis785@users.noreply.github.com>
2022-06-02 19:56:52 +0100

    ADD NameMassInput component

---
+++ frontend/src/components/NameMassInput.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { NameMass } from '../types'
+
+interface Props {
+  values: NameMass
+  onChange: (values: NameMass) => void
+  onRemoveClick?: (e: React.MouseEvent) => void
+}
+
+export const NameMassInput = ({ values, onChange, onRemoveClick }: Props) => {
+  const { name, mass } = values
+
+  type InputEvent = React.ChangeEvent<HTMLInputElement>
+
+  const onMassChange = (e: InputEvent) => {
+    const value = parseFloat(e.target.value)
+    console.log({ mass, value, event: e.target.value })
+    if (isNaN(value)) {
+      return onChange({ name, mass })
+    }
+    onChange({ name, mass: value })
+  }
+
+  return (
+    <span>
+      <input
+        type="text"
+        value={name}
+        onChange={(e: InputEvent) => onChange({ name: e.target.value, mass })}
+      />
+      <input type="number" step="0.01" value={mass} onChange={onMassChange} />
+      <button onClick={onRemoveClick}>Remove Row</button>
+    </span>
+  )
+}
