d7bb8a3e3515fbdb9277c96ad93afde0d70f92a9
Lewis McNeill <6698050+lewis785@users.noreply.github.com>
2022-06-13 13:30:39 +0100

    ADD InputForm stores value changes

--- frontend/src/components/InputForm.tsx
+++ frontend/src/components/InputForm.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import styled from 'styled-components'
+import { Input } from '../types'
 import { ConfigFields } from './ConfigFields'
 import { NameMassForm } from './NameMassForm'
 
@@ -7,12 +8,26 @@ const Form = styled.form`
   display: flex;
 `
 
-export const InputForm = () => {
+interface Props {
+  onValueChange: (value: Input) => void
+}
+
+export const InputForm = ({ onValueChange }: Props) => {
   const [monomers, setMonomers] = useState({})
   const [catIons, setCatIons] = useState({})
   const [endGroups, setEndGroups] = useState({})
   const [totalMass, setTotalMass] = useState(0)
   const [difference, setDifference] = useState(0)
+
+  useEffect(() => {
+    onValueChange({
+      totalMass,
+      difference,
+      monomers: Object.values(monomers),
+      cations: Object.values(catIons),
+      endGroups: Object.values(endGroups),
+    })
+  }, [monomers, catIons, endGroups, totalMass, difference])
 
   const onConfigChange = (config: {
     totalMass: number
@@ -30,16 +45,19 @@ export const InputForm = () => {
         onChange={onConfigChange}
       />
       <NameMassForm
+        key="monomers"
         rows={monomers}
         onChange={(rows) => setMonomers({ ...monomers, ...rows })}
         rowCount={1}
       />
       <NameMassForm
+        key="catIons"
         rows={catIons}
         onChange={(rows) => setCatIons({ ...catIons, ...rows })}
         rowCount={3}
       />
       <NameMassForm
+        key="endGroups"
         rows={endGroups}
         onChange={(rows) => setEndGroups({ ...endGroups, ...rows })}
         rowCount={3}
