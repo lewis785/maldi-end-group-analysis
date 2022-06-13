38cdf522e630e3a4d602e0ede73e1ee2001474f1
Lewis McNeill <6698050+lewis785@users.noreply.github.com>
2022-06-13 13:29:19 +0100

    ADD Input type

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -2,3 +2,11 @@ export interface NameMass {
   name: string
   mass: number
 }
+
+export interface Input {
+  totalMass: number
+  difference: number
+  monomers: NameMass[]
+  endGroups: NameMass[]
+  cations: NameMass[]
+}
