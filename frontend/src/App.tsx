97e4cd0090911d839c7cc70bca5268718860fa3e
Lewis McNeill <6698050+lewis785@users.noreply.github.com>
2022-06-14 18:19:21 +0100

    ADD basic page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,26 @@
+import { useState } from 'react'
+import { InputForm } from './components/InputForm'
+import { useMaldi } from './hooks/useMaldi'
+import { Input } from 'maldi-end-group-analysis'
+import { Results } from './components/Results'
+
 function App() {
-  return <div className="App"></div>
+  const [input, setInput] = useState<Input>()
+  const [generateResult, result] = useMaldi()
+
+  const onClick = () => {
+    if (input !== undefined) {
+      generateResult(input)
+    }
+  }
+
+  return (
+    <div className="App">
+      <InputForm onValueChange={(value) => setInput(value)} />
+      <button onClick={onClick} />
+      <Results results={result} />
+    </div>
+  )
 }
 
 export default App
