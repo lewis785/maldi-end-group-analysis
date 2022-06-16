35d6e86004f3ea1f30b43dab6725d020db041018
Lewis McNeill <6698050+lewis785@users.noreply.github.com>
2022-06-16 14:55:55 +0100

    ADD storing results in local session

--- frontend/src/hooks/useMaldi.ts
+++ frontend/src/hooks/useMaldi.ts
@@ -1,8 +1,14 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { generateMaldiResults, Input, Result } from 'maldi-end-group-analysis'
 
 export const useMaldi = (): [(input: Input) => void, Result[]] => {
-  const [result, setResult] = useState<Result[]>([])
+  const [result, setResult] = useState<Result[]>(
+    JSON.parse(sessionStorage.getItem('maldi-results') || '[]')
+  )
+
+  useEffect(() => {
+    sessionStorage.setItem('maldi-results', JSON.stringify(result))
+  }, [result])
 
   const generateResult = (input: Input) => {
     setResult(generateMaldiResults(input, 0))
