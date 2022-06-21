d1e2a4aa204ba1c461b0563e6e925b535459ee17
Lewis McNeill <6698050+lewis785@users.noreply.github.com>
2022-06-21 22:40:51 +0100

    ADD error prop to DecimalInput component

--- frontend/src/components/DecimalInput.tsx
+++ frontend/src/components/DecimalInput.tsx
@@ -1,27 +1,43 @@
 import { ChangeEvent, useState } from 'react'
+import styled from 'styled-components'
+
+const Input = styled.input`
+  &.error {
+    border: 1px solid red;
+  }
+`
 
 interface Props {
   decimalPlaces: number
   initialValue: number | null
+  error?: boolean
   onChange: (value: string) => void
 }
 
 export const DecimalInput = ({
-  initialValue,
   decimalPlaces,
+  initialValue,
+  error = false,
   onChange,
 }: Props) => {
-  const regex = new RegExp(`^\\d+(\\.\\d{0,${decimalPlaces}})?$`)
+  const decimalRegex = new RegExp(`^\\d+(\\.\\d{0,${decimalPlaces}})?$`)
   const [decimalValue, setDecimalValue] = useState<string>(
     initialValue ? initialValue.toFixed(decimalPlaces) : ''
   )
 
   const onValueChange = ({ target }: ChangeEvent<HTMLInputElement>) => {
-    if (!target.value || target.value.match(regex)) {
+    if (!target.value || target.value.match(decimalRegex)) {
       setDecimalValue(target.value)
       onChange(target.value)
     }
   }
 
-  return <input type="text" value={decimalValue} onChange={onValueChange} />
+  return (
+    <Input
+      className={`${error ? 'error' : ''}`}
+      type="text"
+      value={decimalValue}
+      onChange={onValueChange}
+    />
+  )
 }
