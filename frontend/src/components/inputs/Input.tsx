f887c3d7a6e1ea46b463134ada5251b2fdf92d6f
Lewis McNeill <6698050+lewis785@users.noreply.github.com>
2022-12-08 23:24:05 +0000

    CHANGE input style

--- frontend/src/components/inputs/Input.tsx
+++ frontend/src/components/inputs/Input.tsx
@@ -4,9 +4,8 @@ interface Props extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const InputField = styled.input`
   padding: 0.5rem;
-  border-radius: 8px;
-  border: 1px solid rgba(0, 0, 0, 0.8);
-  box-shadow: 0 0 5pt 0.5pt rgb(211, 211, 211);
+  border-radius: 2px;
+  border: 1px solid rgba(0, 0, 0, 0.4);
   background: rgb(255, 255, 255);
 
   &.error {
