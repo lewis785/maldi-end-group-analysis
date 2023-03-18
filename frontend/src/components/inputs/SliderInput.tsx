56c7207ee42b56604d4c51f96eecb313d44bfb87
Lewis McNeill <6698050+lewis785@users.noreply.github.com>
2023-03-18 16:13:58 +0000

    CHANGE styling of slider input

--- frontend/src/components/inputs/SliderInput.tsx
+++ frontend/src/components/inputs/SliderInput.tsx
@@ -11,6 +11,21 @@ interface Props {
 const HStack = styled.div`
   display: flex;
   flex-direction: row;
+  gap: 0.5rem;
+  align-items: center;
+`
+
+const SliderTextInput = styled.input`
+  width: 2rem;
+  border-radius: 10px;
+  text-align: center;
+  padding: 0.5rem;
+  border: 1px solid rgba(0, 0, 0, 0.4);
+  min-width: 0;
+`
+
+const Slider = styled.input`
+  min-width: 0;
 `
 
 export const SliderInput = ({ max, min, value, onChange }: Props) => {
@@ -33,11 +48,14 @@ export const SliderInput = ({ max, min, value, onChange }: Props) => {
 
   return (
     <HStack>
-      <Input
+      <label htmlFor="slider">Difference</label>
+      <SliderTextInput
+        id="slider"
+        type="text"
         value={value}
         onChange={(e) => onChange(convertToNumber(e.target.value))}
       />
-      <input
+      <Slider
         type="range"
         min={min}
         max={max}
