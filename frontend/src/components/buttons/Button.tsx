961cf6227db6ce78f77efc9c177742e17ed06969
Lewis McNeill <6698050+lewis785@users.noreply.github.com>
2023-03-18 16:10:42 +0000

    ADD button component

---
+++ frontend/src/components/buttons/Button.tsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components'
+
+interface Props {
+  onClickHandler: () => void
+  children: React.ReactNode
+  disabled?: boolean
+}
+
+const StyledButton = styled.button`
+  padding: 0.5rem 2rem;
+  border: 0;
+  border-radius: 8px;
+  background: ${({ theme }) => theme.primary.action};
+  font-size: 1rem;
+
+  &:hover {
+    cursor: pointer;
+    background: #0085a2;
+  }
+
+  &:disabled {
+    cursor: not-allowed;
+  }
+`
+
+export const Button = ({
+  onClickHandler,
+  children,
+  disabled = false,
+}: Props) => {
+  return (
+    <StyledButton onClick={onClickHandler} disabled={disabled}>
+      {children}
+    </StyledButton>
+  )
+}
