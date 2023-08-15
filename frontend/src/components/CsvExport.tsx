3aa6b2dd3e6a12c8d3e038742e3446b61cfc0e35
Lewis McNeill <6698050+lewis785@users.noreply.github.com>
2023-08-15 18:43:07 +0100

    FIX parsed input causing export error

--- frontend/src/components/CsvExport.tsx
+++ frontend/src/components/CsvExport.tsx
@@ -2,7 +2,7 @@ import { Input, Result } from 'maldi-end-group-analysis'
 import { Button } from './buttons/Button'
 import { csv } from 'maldi-end-group-analysis'
 import { CSVLink } from 'react-csv'
-import { useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { parseInput } from '../utils/parseInput'
 
 interface Props {
@@ -10,10 +10,24 @@ interface Props {
   results: Result[]
 }
 
+const isValidInput = (input: Input) => {
+  return (
+    input.peaks.length > 0 &&
+    input.monomers.length > 0 &&
+    input.cations.length > 0 &&
+    input.endGroups.length > 0
+  )
+}
+
 export const CsvExport = ({ input, results }: Props) => {
   const ref = useRef<CSVLink & HTMLAnchorElement & { link: HTMLAnchorElement }>(
     null
   )
+
+  const parsedInput = parseInput(input)
+  const data = isValidInput(parsedInput)
+    ? csv.exportToArray(parsedInput, results)
+    : []
 
   const csvExport = () => {
     ref.current?.link.click()
@@ -24,12 +38,7 @@ export const CsvExport = ({ input, results }: Props) => {
       <Button onClickHandler={csvExport} disabled={results.length === 0}>
         Export
       </Button>
-      <CSVLink
-        ref={ref}
-        filename="maldi-end-group-analysis"
-        data={csv.exportToArray(parseInput(input), results)}
-        hidden
-      >
+      <CSVLink ref={ref} filename="maldi-end-group-analysis" data={data} hidden>
         Export
       </CSVLink>
     </>
