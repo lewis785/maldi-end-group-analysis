d7ee81bb7e6d8a06be1e0b7f36b5853fdf8a99a7
Lewis McNeill <6698050+lewis785@users.noreply.github.com>
2023-09-04 23:59:51 +0100

    REFACTOR remove useEffect from useMaldi hook

--- frontend/src/hooks/useMaldi.ts
+++ frontend/src/hooks/useMaldi.ts
@@ -1,21 +1,21 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { generateMaldiResults, Input, Result } from 'maldi-end-group-analysis'
 import { parseInput } from '../utils/parseInput'
+
+const resultsKey = 'maldi-results'
 
 export const useMaldi = (): [
   (input: Input, difference: number) => void,
   Result[]
 ] => {
   const [result, setResult] = useState<Result[]>(
-    JSON.parse(sessionStorage.getItem('maldi-results') || '[]')
+    JSON.parse(sessionStorage.getItem(resultsKey) || '[]')
   )
 
-  useEffect(() => {
-    sessionStorage.setItem('maldi-results', JSON.stringify(result))
-  }, [result])
-
   const generateResult = (input: Input, difference: number) => {
-    setResult(generateMaldiResults(parseInput(input), difference))
+    const newResult = generateMaldiResults(parseInput(input), difference)
+    setResult(newResult)
+    sessionStorage.setItem(resultsKey, JSON.stringify(newResult))
   }
 
   return [generateResult, result]
